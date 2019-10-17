7e31740875fc66e961736f5d6ffde3208e90983c
Daniel Kondow <danielkondov91@gmail.com>
2019-10-17 09:59:14 +0300

    Starting with the First Bounded Context

---
+++ DddInPractice/DomainDrivenDesign/DomainDrivenDesign.Logic/SnackMachine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using static DomainDrivenDesign.Logic.Money;
+
+namespace DomainDrivenDesign.Logic
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money MoneyInSide { get; private set; } = None;
+
+        public Money MoneyInTransaction { get; private set; } = None;
+
+        public void InsertMoney(Money money)
+        {
+            Money[] coinsAndNotes =
+            {
+                Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar
+            };
+            if (!coinsAndNotes.Contains(money))
+                throw new InvalidOperationException();
+
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+            MoneyInTransaction = None;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInSide += MoneyInTransaction;
+            MoneyInTransaction = None;
+        }
+    }
+}
