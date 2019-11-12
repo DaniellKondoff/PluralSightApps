f60993ee8e48d04e6774f9feb1870319d4ff32e3
Daniel Kondow <danielkondov91@gmail.com>
2019-11-12 20:29:24 +0200

    PluralSight

---
+++ SignalR/demos/WiredBrain/Hubs/CoffeeHub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using WiredBrain.Helpers;
+
+namespace WiredBrain.Hubs
+{
+    public class CoffeeHub: Hub
+    {
+        private readonly OrderChecker _orderChecker;
+
+        public CoffeeHub(OrderChecker orderChecker)
+        {
+            _orderChecker = orderChecker;
+        }
+
+        public async Task GetUpdateForOrder(int orderId)
+        {
+            CheckResult result;
+            do
+            {
+                result = _orderChecker.GetUpdate(orderId);
+                Thread.Sleep(1000);
+                if (result.New)
+                    await Clients.Caller.SendAsync("ReceiveOrderUpdate", 
+                        result.Update);
+            } while (!result.Finished);
+            await Clients.Caller.SendAsync("Finished");
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ SignalR/demos/WiredBrain/Hubs/TestHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WiredBrain.Hubs
+{
+    public class TestHub : Hub
+    {
+        public TestHub()
+        {
+
+        }
+
+        public async Task<bool> GetUpdateForOrder()
+        {
+            return await Task.FromResult(true);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+    }
+}
