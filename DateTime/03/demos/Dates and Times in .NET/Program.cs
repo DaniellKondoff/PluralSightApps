e4b04f63bfeab57ffad369e0f4b92ff36f0330c9
Daniel Kondow <danielkondov91@gmail.com>
2020-04-04 18:50:23 +0300

    Datetime

--- DateTime/03/demos/Dates and Times in .NET/Program.cs
+++ DateTime/03/demos/Dates and Times in .NET/Program.cs
@@ -54,6 +54,80 @@ namespace Dates_and_Times_in_.NET
 
             Console.WriteLine(contractDate);
             #endregion
+
+            #region Timespan
+            Console.WriteLine();
+            Console.WriteLine("TimeSpan Section");
+            TimeSpan span = new TimeSpan(1, 2, 0, 30, 0);
+            Console.WriteLine(span);
+
+            //Add
+            TimeSpan span1 = TimeSpan.FromMinutes(1);
+            TimeSpan span2 = TimeSpan.FromMinutes(2);
+            TimeSpan span3 = span1.Add(span2);
+            Console.WriteLine(span3);
+
+            //Duration
+            Console.WriteLine($"Durations: {span}");
+            Console.WriteLine($"Durations: {span3}");
+
+            //Hours, TotalHours.
+            TimeSpan span4 = new TimeSpan(0, 500, 0, 0, 0);
+            Console.WriteLine("Hours: " + span4.Hours);
+            Console.WriteLine("Total Hours: " + span4.TotalHours);
+
+            // Demonstrate TimeSpan zero.
+            TimeSpan span5 = TimeSpan.Zero;
+            Console.WriteLine(span5);
+            Console.WriteLine(span5.TotalMilliseconds);
+
+
+            // Define two dates.
+            DateTime date1 = new DateTime(2010, 1, 1, 8, 0, 15);
+            DateTime date2 = new DateTime(2010, 8, 18, 13, 30, 30);
+
+            // Calculate the interval between the two dates.
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine("{0} - {1} = {2}", date2, date1, interval.ToString());
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine(now.AddTicks(interval.Ticks));
+            Console.WriteLine(date2.AddTicks(-interval.Ticks));
+
+            // Display individual properties of the resulting TimeSpan object.
+            Console.WriteLine("   {0,-35} {1,20}", "Value of Days Component:", interval.Days);
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Days:", interval.TotalDays);
+            Console.WriteLine("   {0,-35} {1,20}", "Value of Hours Component:", interval.Hours);
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Hours:", interval.TotalHours);
+            Console.WriteLine("   {0,-35} {1,20}", "Value of Minutes Component:", interval.Minutes);
+            Console.WriteLine("   {0,-35} {1,20}", "Total Number of Minutes:", interval.TotalMinutes);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Seconds Component:", interval.Seconds);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Total Number of Seconds:", interval.TotalSeconds);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Value of Milliseconds Component:", interval.Milliseconds);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Total Number of Milliseconds:", interval.TotalMilliseconds);
+            Console.WriteLine("   {0,-35} {1,20:N0}", "Ticks:", interval.Ticks);
+
+            #endregion
+
+            #region Tick
+            Console.WriteLine();
+            DateTime centuryBegin = new DateTime(2001, 1, 1);
+            DateTime currentDate = DateTime.Now;
+
+            long elapsedTicks = currentDate.Ticks - centuryBegin.Ticks;
+            TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
+
+            Console.WriteLine("Elapsed from the beginning of the century to {0:f}:",
+                               currentDate);
+            Console.WriteLine("   {0:N0} nanoseconds", elapsedTicks * 100);
+            Console.WriteLine("   {0:N0} ticks", elapsedTicks);
+            Console.WriteLine("   {0:N2} seconds", elapsedSpan.TotalSeconds);
+            Console.WriteLine("   {0:N2} minutes", elapsedSpan.TotalMinutes);
+            Console.WriteLine("   {0:N0} days, {1} hours, {2} minutes, {3} seconds",
+                              elapsedSpan.Days, elapsedSpan.Hours,
+                              elapsedSpan.Minutes, elapsedSpan.Seconds);
+            #endregion
         }
 
         public static DateTimeOffset ExtendContract(DateTimeOffset current, int months)
