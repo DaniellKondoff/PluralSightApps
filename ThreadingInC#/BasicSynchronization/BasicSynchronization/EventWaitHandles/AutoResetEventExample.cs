900e783f565faf7e74b9b37d840a7c8688c01c0b
Daniel Kondow <danielkondov91@gmail.com>
2020-04-05 19:45:22 +0300

    Basic Sync

---
+++ ThreadingInC#/BasicSynchronization/BasicSynchronization/EventWaitHandles/AutoResetEventExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace EventWaitHandles
+{
+    public class AutoResetEventExample
+    {
+        static EventWaitHandle _waitHandle = new AutoResetEvent(false);
+
+
+        public static void WakeUp()
+        {
+            _waitHandle.Set();
+        }
+
+        public static void Waiter()
+        {
+            Console.WriteLine("Waiting...");
+            _waitHandle.WaitOne();                // Wait for notification
+            Console.WriteLine("Notified");
+        }
+    }
+}
---
+++ ThreadingInC#/BasicSynchronization/BasicSynchronization/EventWaitHandles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace EventWaitHandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(AutoResetEventExample.Waiter).Start();
+            Console.ReadKey();
+            AutoResetEventExample.WakeUp();
+
+
+            for (int i = 1; i <= 2; i++)
+            {
+                new Thread(CountdownEventExample.SaySomething).Start("I am thred " + i);
+            }
+            CountdownEventExample.Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
