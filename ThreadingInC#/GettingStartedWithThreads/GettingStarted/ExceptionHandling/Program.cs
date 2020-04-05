718f90f1502f7abf297fc847c122fb618dd41fe1
Daniel Kondow <danielkondov91@gmail.com>
2020-04-05 14:50:10 +0300

    GettingStarted Part

---
+++ ThreadingInC#/GettingStartedWithThreads/GettingStarted/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(Go);
+
+            t.Start();
+
+            try
+            {
+                new Thread(Go).Start();
+            }
+            catch (Exception ex)
+            {
+                // We'll never get here!
+                Console.WriteLine("Exception in main method!");
+            }
+           
+        }
+
+        static void Go()
+        {
+            try
+            {
+               
+                throw null;    // The NullReferenceException will get caught below
+                               // ...
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in Thread Method catched");
+            }
+        }
+    }
+}
