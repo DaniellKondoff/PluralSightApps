5953c16eb737dda232ab8d9dc7130f8db3f40048
Daniel Kondow <danielkondov91@gmail.com>
2020-04-05 22:41:18 +0300

    task wit cancelltoken

---
+++ ThreadingInC#/ThreadingInCSharp/TaskWithCencalation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace TaskWithCencalation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Thred ID: " + Thread.CurrentThread.ManagedThreadId);
+            Worker worker = new Worker();
+            System.Timers.Timer tmr = new System.Timers.Timer();
+            tmr.Interval = 5000;
+            tmr.Elapsed += worker.tmr_Elapsed;
+            tmr.Start();
+            Console.ReadLine();
+            tmr.Stop();
+            Console.WriteLine("After Main Method Thred ID: " + Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
